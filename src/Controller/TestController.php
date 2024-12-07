3716d240c6fcc43842f5ecd0f130e29029b9a507
ayPromise servant <fatherpother@gmail.com>
2024-12-07 17:35:51 +0200

    Lab 1 - added 4 CRUD operations

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -7,13 +7,33 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
+ #[Route('/test')]
 class TestController extends AbstractController
 {
-    #[Route('/test', name: 'app_test', methods:["POST"])]
-    public function index(Request $request): JsonResponse
+    #[Route('', name: 'app_test_get', methods:["GET"])]
+    public function get(Request $request): JsonResponse
     {
-        $querryParams = $request->query->all();
-        dd($querryParams); 
+        $querryParams = $request->query->all(); 
+        return new JsonResponse($querryParams);
+    }
+
+    #[Route('/post', name:'app_test_post', methods:["POST"])]
+    public function post(Request $request): JsonResponse
+    {
+        $requestBody = json_decode($request->getContent(), true);
+        return new JsonReponse($requestBody);
+    }
+
+
+    #[Route('/getSomething', name:'app_test_get_something', methods:["GET"])]
+    public function getItem(Request $request) : JsonResponse
+    {
         return new JsonResponse();
+    }
+
+    #[Route('/helloWorld', name:'app_test_hello_world', methods:["GET"])]
+    public function getHelloWorld() : JsonResponse
+    {
+        return new JsonResponse("Hello World!");
     }
 }
